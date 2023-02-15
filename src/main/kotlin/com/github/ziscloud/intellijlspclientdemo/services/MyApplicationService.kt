b54e012a66557b24a9407bd07888eca5f1ae2c3d
GitHub Action <action@github.com>
2023-02-15 06:37:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/ziscloud/intellijlspclientdemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ziscloud/intellijlspclientdemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ziscloud.intellijlspclientdemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ziscloud.intellijlspclientdemo.MyBundle
 
 class MyApplicationService {
 
